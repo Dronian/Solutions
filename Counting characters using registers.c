09658ffdf075cc999071b9959302be87c788c33c
Dronian <andreyivfgh567@gmail.com>
2025-10-14 12:47:48 +0400

    Update Counting characters using registers.c

--- Counting characters using registers.c
+++ Counting characters using registers.c
@@ -6,7 +6,7 @@ int main()
     int charc[MAX] = {0}; 
     int qc;
     
-    while ((qc = getchar()) != '\n') { //Если EOF, вместо '\n', то выводит вхождение EOF?
+    while ((qc = getchar()) != '\n') {    // Если EOF, вместо '\n', то выводит вхождение EOF?
          if (qc >= 'A' && qc <= 'Z') {
             qc = qc - 'A' + 'a';
         }
@@ -21,3 +21,4 @@ int main()
 
     return 0;
 }
+
